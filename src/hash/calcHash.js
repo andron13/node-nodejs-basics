e903631c49d1165e86362ebf3d78687a6929a714
andron13 <andron13@gmail.com>
2023-06-14 16:09:44 +0200

    feat: implement src/hash: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import crypto from "crypto";
+import {fileURLToPath} from "url";
+import {join} from "path";
+
+const errorText = "The SHA256 hash calculating operation failed.";
+const file = "fileToCalculateHashFor.txt";
+const folder = "files";
+const filePath = join(fileURLToPath(import.meta.url), "..", folder, file);
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const data = await fs.promises.readFile(filePath);
+    const hash = crypto.createHash("sha256").update(data).digest("hex");
+    console.log(hash);
+  } catch (err) {
+    console.error(errorText, err);
+  }
 };
 
 await calculateHash();
