be32be8145a0c436378f821603b2aa5f1508c205
andron13 <andron13@gmail.com>
2023-06-14 16:34:01 +0200

    feat: implement src/streams in terminal: read, write, transform

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,32 @@
+import fs from 'fs';
+import {join} from "path";
+import {fileURLToPath} from "url";
+
+const errorText = "\n Readable Stream Write Error";
+const finalText = "\n File writing completed.";
+const file = "fileToWrite.txt";
+const folder = "files";
+const filePath = join(fileURLToPath(import.meta.url), "..", folder, file);
+
 const write = async () => {
-    // Write your code here 
+
+  const writableStream = fs.createWriteStream(filePath);
+
+  process.stdin.on('data', (chunk) => {
+    writableStream.write(chunk);
+  });
+
+  process.stdin.on('end', () => {
+    writableStream.end();
+  });
+
+  writableStream.on('finish', () => {
+    console.log(finalText);  //  Ctrl + D - Mac, Ctrl + Z - Win
+  });
+
+  writableStream.on('error', (err) => {
+    console.error(errorText, err.message);
+  });
 };
 
 await write();
