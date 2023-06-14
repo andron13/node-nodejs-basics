8e41a446dc8f357a924fcdfd7935b3c7cb35f4cd
andron13 <andron13@gmail.com>
2023-06-14 15:30:57 +0200

    feat: implement src/cli: args.js, env.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const result = args
+    .map((arg, index) => {
+      if (arg.startsWith('--')) {
+        const key = arg.slice(2);
+        const value = args[index + 1];
+        return `${key} is ${value}`;
+      }
+    })
+    .filter(Boolean)
+    .join(', ');
+
+  console.log(result);
 };
 
 parseArgs();
+
+// Test:$node args.js --name John --age 30 --city London
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,29 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envs = process.env;
+  const filteredEnvs = Object.entries(envs)
+    .filter(([key]) => key.startsWith('RSS_'));
+
+  if (filteredEnvs.length === 0) {
+    console.log('No RSS environment variables found.');
+    return;
+  }
+  const result = filteredEnvs
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+  console.log(result);
 };
 
 parseEnv();
+
+/**
+ * The testParseEnv function creates several environment variables with the prefix 'RSS_'
+ * and calls the parseEnv function to process them and print them to the console.
+ * This is a test function to verify the correctness of the parseEnv function
+ * according to the given technical requirements.
+ */
+const testParseEnv = () => {
+  process.env.RSS_name1 = 'value1';
+  process.env.RSS_name2 = 'value2';
+  process.env.RSS_name3 = 'value3';
+  parseEnv();
+};
