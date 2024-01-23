ca2aeaff96311066f33c25107c78b9e0803eea02
andron13 <andron13@gmail.com>
2024-01-23 17:52:43 +0100

    feat: implement task 2024 calcHach

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -4,18 +4,25 @@ import {fileURLToPath} from "url";
 import {join} from "path";
 
 const errorText = "The SHA256 hash calculating operation failed.";
+const hashAlgorithm = "sha256";
 const file = "fileToCalculateHashFor.txt";
 const folder = "files";
 const filePath = join(fileURLToPath(import.meta.url), "..", folder, file);
 
 const calculateHash = async () => {
-  try {
-    const data = await fs.promises.readFile(filePath);
-    const hash = crypto.createHash("sha256").update(data).digest("hex");
-    console.log(hash);
-  } catch (err) {
-    console.error(errorText, err);
-  }
+  const hash = crypto.createHash(hashAlgorithm);
+  const data = fs.createReadStream(filePath);
+
+  data.on('error', (err) => {
+    process.stderr.write(`${errorText} ${err}\n`);
+  });
+
+  data
+    .pipe(hash)
+    .setEncoding('hex')
+    .on('finish', () => {
+      process.stdout.write(hash.read().toString('hex') + '\n');
+    });
 };
 
 await calculateHash();
